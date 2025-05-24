debd490255dfd52bdfe62cc861820a7397aaa2eb
Anton <antxnlasalle@gmail.com>
2025-05-24 20:50:03 +0200

    preus

--- vorhees.js
+++ vorhees.js
@@ -1,262 +1,298 @@
 const vorheesData = [
   {
-    "id": "1",
-    "titol": "Elden Ring",
-    "items": "372 items",
-    "descripcio": "Descripcio elden ring",
-    "imatge": "img/imatges jocs/eldenring.jpg",
+    id: "1",
+    titol: "Elden Ring",
+    items: "372 items",
+    descripcio: "Descripcio elden ring",
+    imatge: "img/imatges jocs/eldenring.jpg",
     productes: [
       {
         nom: "Espasa de la Llum (sword of light)",
         imatge: "img/items/eldenring/swordoflight.jpg",
-        categoria: "Armes" 
+        categoria: "Armes",
+        preu: "1200"
       },
       {
         nom: "Dits estimats (cherishing fingers)",
         imatge: "img/items/eldenring/cherishingfingers.jpg",
-        categoria: "Ecanteris"
+        categoria: "Ecanteris",
+        preu: "800"
       },
       {
         nom: "Màscara d'Albinàuric (albinauric mask)",
         imatge: "img/items/eldenring/albinauricmask.jpg",
-        categoria: "Armadures"
+        categoria: "Armadures",
+        preu: "650"
       },
       {
         nom: "Seppuku (Seppuku)",
         imatge: "img/items/eldenring/seppuku.jpg",
-        categoria: "Cendres de guerra"
+        categoria: "Cendres de guerra",
+        preu: "900"
       }
     ]
   },
   {
-    "id": "2",
-    "titol": "Team Fortress 2",
-    "items": "3000+ items",
-    "descripcio": "",
-    "imatge": "img/imatges jocs/teamfortress.png",
+    id: "2",
+    titol: "Team Fortress 2",
+    items: "3000+ items",
+    descripcio: "",
+    imatge: "img/imatges jocs/teamfortress.png",
     productes: [
       {
         nom: "Llet boja (Mad milk)",
         imatge: "img/items/tf2/madmilk.jpg",
-        categoria: "Consumible"
+        categoria: "Consumible",
+        preu: "200"
       },
       {
         nom: "Les botes d'Ali Baba (Ali Baba's wee booties)",
         imatge: "img/items/tf2/alibababoots.jpg",
-        categoria: "Cosmètics"
+        categoria: "Cosmètics",
+        preu: "450"
       },
       {
         nom: "Sobredosi (Overdose)",
         imatge: "img/items/tf2/overdose.jpg",
-        categoria: "Armes"
+        categoria: "Armes",
+        preu: "300"
       },
       {
         nom: "Terroràntula pintada (Painted Terror-antula)",
         imatge: "img/items/tf2/terrorantula.jpg",
-        categoria: "Cosmètics"
+        categoria: "Cosmètics",
+        preu: "750"
       }
     ]
   },
   {
-    "id": "3",
-    "titol": "Dota2",
-    "items": "208 items",
-    "descripcio": "",
-    "imatge": "img/imatges jocs/dota2.jpg",
+    id: "3",
+    titol: "Dota2",
+    items: "208 items",
+    descripcio: "",
+    imatge: "img/imatges jocs/dota2.jpg",
     productes:[
       {
         nom: "Fulla astuta (Swift Blink)",
         imatge: "img/items/dota2/swiftblink.jpg",
-        categoria: "Arma"
+        categoria: "Arma",
+        preu: "1300"
       },
       {
         nom: "Vel de discòrdia (Veil of Discord)",
         imatge: "img/items/dota2/veilofdiscord.jpg",
-        categoria: "Armadura"
+        categoria: "Armadura",
+        preu: "950"
       },
       {
         nom: "Botes Arcanes (Arcane Boots)",
         imatge: "img/items/dota2/arcaneboots.jpg",
-        categoria: "Armadura"
+        categoria: "Armadura",
+        preu: "400"
       },
       {
         nom: "Fragment de lluna (Moon Shard)",
         imatge: "img/items/dota2/moonshard.jpg",
-        categoria: "Artefactes"
+        categoria: "Artefactes",
+        preu: "700"
       }
     ]
   },
   {
-    "id": "4",
-    "titol": "OldSchoolRuneScape",
-    "items": "39000+ items",
-    "descripcio": "",
-    "imatge": "img/imatges jocs/runescape.jpg",
+    id: "4",
+    titol: "RuneScape",
+    items: "39000+ items",
+    descripcio: "",
+    imatge: "img/imatges jocs/runescape.jpg",
     productes:[
       {
         nom: "Cimitarra rúnica (Rune scimitar)",
         imatge: "img/items/runescape/runescimitar.jpg",
-        categoria: "Armes"
+        categoria: "Armes",
+        preu: "150"
       },
       {
         nom: "Última voluntat i testament (Last will and testament)",
         imatge: "img/items/runescape/lastwill.jpg",
-        categoria: "Miscelànea"
+        categoria: "Miscelànea",
+        preu: "90"
       },
       {
         nom: "Caputxa del segador (Grim reaper hood)",
         imatge: "img/items/runescape/grimreaperhood.jpg",
-        categoria: "Miscelànea"
+        categoria: "Miscelànea",
+        preu: "300"
       },
       {
         nom: "Carta d'amor (Love Letter)",
         imatge: "img/items/runescape/loveletter.jpg",
-        categoria: "Miscelànea"
+        categoria: "Miscelànea",
+        preu: "50"
       }
     ]
   },
   {
-    "id": "5",
-    "titol": "Diablo 4",
-    "items": "300+",
-    "descripcio": "",
-    "imatge": "img/imatges jocs/d4.jpg",
+    id: "5",
+    titol: "Diablo 4",
+    items: "300+ items",
+    descripcio: "",
+    imatge: "img/imatges jocs/d4.jpg",
     productes:[
       {
         nom: "Cresta d'Arlequí (Harlequin crest)",
         imatge: "img/items/diablo4/harlequincrest.jpg",
-        categoria: "Mythic Unique"
+        categoria: "Mythic Unique",
+        preu: "3200"
       },
       {
         nom: "El poder de Tyrael (Tyrael's Mighty)",
         imatge: "img/items/diablo4/tyraelmight.jpg",
-        categoria: "Mythic Unique"
+        categoria: "Mythic Unique",
+        preu: "2900"
       },
       {
         nom: "Vestit de l'Infinit (Raiment of the Infinite)",
         imatge: "img/items/diablo4/raimentinfinite.jpg",
-        categoria: "Uniques"
+        categoria: "Uniques",
+        preu: "2100"
       },
       {
         nom: "Catalitzador d'Okun (Okun's Catalyst)",
         imatge: "img/items/diablo4/okuncatalyst.jpg",
-        categoria: "Catalyst"
+        categoria: "Catalyst",
+        preu: "1800"
       }
     ]
   },
   {
-    "id": "6",
-    "titol": "CSGO2",
-    "items": "11130+ items",
-    "descripcio": "",
-    "imatge": "img/imatges jocs/csgo2.jpg",
+    id: "6",
+    titol: "CSGO2",
+    items: "11130+ items",
+    descripcio: "",
+    imatge: "img/imatges jocs/csgo2.jpg",
     productes: [
       {
         nom: "Tec-9 - Nuclear Threat",
         imatge: "img/items/csgo2/tec-9.jpg",
-        categoria: "Pistola semiautomàtica" 
+        categoria: "Pistola semiautomàtica",
+        preu: "1600"
       },
       {
         nom: "AWP - Dragon Lore",
         imatge: "img/items/csgo2/awp.jpg",
-        categoria: "Fusils de tirador"
+        categoria: "Fusils de tirador",
+        preu: "15000"
       },
       {
         nom: "AK-47 - Wild Lotus",
         imatge: "img/items/csgo2/ak47.jpg",
-        categoria: "Fusil d'asalt"
+        categoria: "Fusil d'asalt",
+        preu: "12000"
       },
       {
         nom: "Glock-18 - Fade",
         imatge: "img/items/csgo2/glock-18.jpg",
-        categoria: "Pistola semiautomàtica"
+        categoria: "Pistola semiautomàtica",
+        preu: "2000"
       }
     ]
   },
   {
-    "id": "7",
-    "titol": "World of Warcraft",
-    "items": "65893+ items",
-    "descripcio": "",
-    "imatge": "img/imatges jocs/wow.png",
+    id: "7",
+    titol: "World of Warcraft",
+    items: "65893+ items",
+    descripcio: "",
+    imatge: "img/imatges jocs/wow.png",
     productes: [
       {
         nom: "Pila de trastos oxidats",
         imatge: "img/items/wow/trastosoxidats.jpg",
-        categoria: "Miscelànea" 
+        categoria: "Miscelànea",
+        preu: "40"
       },
       {
         nom: "Baralla de Naipes",
         imatge: "img/items/wow/naipes.jpg",
-        categoria: "Armadura"
+        categoria: "Armadura",
+        preu: "300"
       },
       {
         nom: "Serp negra de N'Zoth",
         imatge: "img/items/wow/serpnegra.jpg",
-        categoria: "Consumible"
+        categoria: "Consumible",
+        preu: "90"
       },
       {
         nom: "Runa de devastació crepuscular superior",
         imatge: "img/items/wow/runasuperior.jpg",
-        categoria: "Consumible"
+        categoria: "Consumible",
+        preu: "120"
       }
     ]
   },
   {
-    "id": "8",
-    "titol": "Escape from Tarkov",
-    "items": "1800+ items",
-    "descripcio": "",
-    "imatge": "img/imatges jocs/tarkov.png",
+    id: "8",
+    titol: "Escape from Tarkov",
+    items: "1800+ items",
+    descripcio: "",
+    imatge: "img/imatges jocs/tarkov.png",
     productes: [
       {
         nom: "Camera",
         imatge: "img/items/tarkov/camera.jpg",
-        categoria: "Miscelànea" 
+        categoria: "Miscelànea",
+        preu: "130"
       },
       {
         nom: "Ram",
         imatge: "img/items/tarkov/ram.jpg",
-        categoria: "Components"
+        categoria: "Components",
+        preu: "180"
       },
       {
         nom: "Cpu",
         imatge: "img/items/tarkov/cpu.jpg",
-        categoria: "Components"
+        categoria: "Components",
+        preu: "250"
       },
       {
         nom: "Sabó",
         imatge: "img/items/tarkov/soap.jpg",
-        categoria: "Consumible"
+        categoria: "Consumible",
+        preu: "60"
       }
     ]
   },
   {
-    "id": "9",
-    "titol": "Fortnite",
-    "items": "7800+ items",
-    "descripcio": "",
-    "imatge": "img/imatges jocs/fortnite.png",
+    id: "9",
+    titol: "Fortnite",
+    items: "7800+ items",
+    descripcio: "",
+    imatge: "img/imatges jocs/fortnite.png",
     productes: [
       {
         nom: "Lanzacoetes",
         imatge: "img/items/fortnite/lanzacoetes.jpg",
-        categoria: "Arma" 
+        categoria: "Arma",
+        preu: "850"
       },
       {
         nom: "Galaxy",
         imatge: "img/items/fortnite/galaxy.jpg",
-        categoria: "Skin"
+        categoria: "Skin",
+        preu: "2000"
       },
       {
         nom: "Lara",
         imatge: "img/items/fortnite/lara.jpg",
-        categoria: "Skin"
+        categoria: "Skin",
+        preu: "1500"
       },
       {
         nom: "Travis",
         imatge: "img/items/fortnite/travis.jpg",
-        categoria: "Skin"
+        categoria: "Skin",
+        preu: "1800"
       }
     ]
   }
